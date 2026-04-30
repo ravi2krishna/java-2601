44b2aaa0959addf7b4fc44fb3b84643dfea2d98a
ravi2krishna <ravi2krishna@gmail.com>
2026-04-30 11:21:45 +0530

    class 2 - Java Setup (JDK) - VS Code - First Program - Rules & Syntax

---
+++ first_basics/Demo.java
@@ -0,0 +1,9 @@
+// This is my first program
+package first_basics; // Folder Name
+
+class Demo {
+    // From Here Program Execution Starts -> main()
+    public static void main(String[] args) { 
+        System.out.println("Say hello");
+    }
+}
